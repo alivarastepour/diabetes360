6a0d697a0a812871d544a9f81f22d6e80675fef3
alivarastepour <alivaraste@outlook.com>
2023-10-19 16:02:12 +0330

    useMediaQuery is being used

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,44 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Tilt_Neon } from "next/font/google";
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
+import HMenu from "./HMenu";
+import RMenu from "./RMenu";
+import useMediaQuery from "@/hooks/useMediaQuery";
 const titltNeon = Tilt_Neon({
   subsets: ["latin"],
   weight: ["400"],
 });
 
 const Header = () => {
+  const [smallScreen] = useMediaQuery({ queries: ["sm-400"] });
   const [hMenuOpen, setHMenuOpen] = useState(false);
+
+  console.log(smallScreen, hMenuOpen);
+
+  useEffect(() => {
+    if (smallScreen === undefined) return;
+    if (!smallScreen) setHMenuOpen(false);
+  }, [smallScreen]);
+
   return (
     <>
-      <div className={styles["header-wrapper"]}>
-        <div className={`${styles["header-logo"]} ${titltNeon.className}`}>
+      <div className={`${titltNeon.className} ${styles["header-wrapper"]}`}>
+        <div className={`${styles["header-logo"]}`}>
           <Link href={"/"}>
             <span>Diabetes</span>
             <span>360</span>
           </Link>
         </div>
         <div className={styles["header-content"]}>
-          <div
-            id="header-h-menu-logo-wrapper"
-            onClick={() => {
-              setHMenuOpen((prev) => !prev);
-            }}
-            className={`${styles["header-h-menu-logo-wrapper"]} ${
-              hMenuOpen
-                ? styles["header-h-menu-open"]
-                : styles["header-h-menu-close"]
-            }`}
-          >
-            <div className={styles["header-h-menu-logo-item"]}></div>
-            <div className={styles["header-h-menu-logo-item"]}></div>
-            <div className={styles["header-h-menu-logo-item"]}></div>
-          </div>
-          {hMenuOpen ? (
-            <div className={styles["h-menu-content"]}>h_menu</div>
-          ) : (
-            <div className={styles["reg-menu-content"]}>reg_menu</div>
+          {smallScreen && (
+            <div
+              id="header-h-menu-logo-wrapper"
+              onClick={() => {
+                setHMenuOpen((prev) => !prev);
+              }}
+              className={`${styles["header-h-menu-logo-wrapper"]} ${
+                hMenuOpen
+                  ? styles["header-h-menu-open"]
+                  : styles["header-h-menu-close"]
+              }`}
+            >
+              <div className={styles["header-h-menu-logo-item"]}></div>
+              <div className={styles["header-h-menu-logo-item"]}></div>
+              <div className={styles["header-h-menu-logo-item"]}></div>
+            </div>
           )}
+          {smallScreen && hMenuOpen && <HMenu />}
+          {!smallScreen && <RMenu />}
         </div>
       </div>
     </>
