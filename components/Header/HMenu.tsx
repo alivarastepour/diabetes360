725364d00d12cdaec9ada6010be0e8b233b4f3dc
alivarastepour <alivaraste@outlook.com>
2023-10-19 16:56:26 +0330

    improved the HMenu styles

--- components/Header/HMenu.tsx
+++ components/Header/HMenu.tsx
@@ -3,7 +3,12 @@ import styles from "@/styles/hMenu.module.scss";
 const HMenu = () => {
   return (
     <>
-      <div className={styles["h-menu-wrapper"]}>HMnue</div>
+      <div className={styles["h-menu-wrapper"]}>
+        <div>what</div>
+        <div>when</div>
+        <div>how</div>
+        <div>test</div>
+      </div>
     </>
   );
 };
