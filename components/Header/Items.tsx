c62279532ffefdce108b502c339ff06e9ce623dc
alivarastepour <alivaraste@outlook.com>
2023-10-19 18:16:47 +0330

    extracted common logic from Header

---
+++ components/Header/Items.tsx
@@ -0,0 +1,14 @@
+import styles from "@/styles/header.items.module.scss";
+
+const Items = () => {
+  return (
+    <>
+      <div className={styles["header-item"]}>what</div>
+      <div className={styles["header-item"]}>when</div>
+      <div className={styles["header-item"]}>how</div>
+      <div className={styles["header-item"]}>test</div>
+    </>
+  );
+};
+
+export default Items;
