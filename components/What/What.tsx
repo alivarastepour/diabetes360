4e588bb9feaa9b0b4d2192e6d2599fb45cbe2116
alivarastepour <alivaraste@outlook.com>
2023-10-20 10:24:38 +0330

    further improved the responsive design

--- components/What/What.tsx
+++ components/What/What.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/what.module.scss";
 import { Tilt_Neon } from "next/font/google";
+import Image from "next/image";
 const titltNeon = Tilt_Neon({
   subsets: ["latin"],
   weight: ["400"],
@@ -13,18 +14,30 @@ const What = () => {
           <span>What </span>
           is diabetes
         </div>
-        <div className={styles["what-content"]}>
-          Diabetes is a chronic medical condition that affects how your body
-          regulates blood sugar (glucose), which is the primary source of energy
-          for your cells. There are several types of diabetes, but the two most
-          common are type 1 and type 2. Type 1 Diabetes is an autoimmune
-          condition in which the body&apos;s immune system mistakenly attacks
-          and destroys the insulin-producing beta cells in the pancreas. Type 2
-          Diabetes is the most common form of diabetes and is often associated
-          with lifestyle and genetic factors. In Type 2 diabetes, the body
-          doesn&apos;t use insulin properly (insulin resistance), and over time,
-          the pancreas may not produce enough insulin. This causes elevated
-          blood sugar levels.
+        <div className={styles["what-content-wrapper"]}>
+          <div className={styles["what-content"]}>
+            Diabetes is a chronic medical condition that affects how your body
+            regulates blood sugar (glucose), which is the primary source of
+            energy for your cells. There are several types of diabetes, but the
+            two most common are type 1 and type 2. Type 1 Diabetes is an
+            autoimmune condition in which the body&apos;s immune system
+            mistakenly attacks and destroys the insulin-producing beta cells in
+            the pancreas. Type 2 Diabetes is the most common form of diabetes
+            and is often associated with lifestyle and genetic factors. In Type
+            2 diabetes, the body doesn&apos;t use insulin properly (insulin
+            resistance), and over time, the pancreas may not produce enough
+            insulin. This causes elevated blood sugar levels.
+          </div>
+          <div className={styles["diabeto"]}>
+            <div>
+              <Image
+                alt="diabeto"
+                src={"/diadio.png"}
+                width={395}
+                height={627}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
