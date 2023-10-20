a4bb65adc769fee2ee5ed4834c18bca5fc8ed291
alivarastepour <alivaraste@outlook.com>
2023-10-20 18:16:13 +0330

    added more data

--- components/How/data.ts
+++ components/How/data.ts
@@ -9,41 +9,42 @@ export const HOW_TO_PREVENT_DATA: IPreventionItem[] = [
     imageAddress: "/healthy_weight.jpg",
     text: "maintain a healthy weight",
     subText:
-      "Aim for a healthy body weight by managing your diet and engaging in regular physical activity. Losing excess weight can significantly reduce your risk of Type 2 diabetes.",
+      "Aim for a healthy body weight by managing your diet and engaging in regular physical activity. Losing excess weight can significantly reduce your risk of Type 2 diabetes. Additionally, maintaining a balanced lifestyle can also improve overall well-being and lower the likelihood of other chronic health conditions.",
   },
   {
     imageAddress: "/physical_activity.avif",
     text: "Regular Physical Activity",
     subText:
-      "Engage in regular exercise to help maintain a healthy weight and improve insulin sensitivity. Aim for at least 150 minutes of moderate-intensity exercise per week.",
+      "Engage in regular exercise to help maintain a healthy weight and improve insulin sensitivity. Aim for at least 150 minutes of moderate-intensity exercise per week. Incorporating regular exercise not only supports weight management and insulin sensitivity but also enhances cardiovascular health and boosts mental well-being.",
   },
   {
     text: "Stay Hydrated",
     imageAddress: "/hydratation.avif",
-    subText: "Drink plenty of water and reduce sugary beverage consumption.",
+    subText:
+      "Drink plenty of water and reduce sugary beverage consumption. Hydration is crucial for your body's optimal functioning, so aim to drink at least 8 cups (about 2 liters) of water per day. Reducing sugary beverage consumption can help lower your daily calorie intake and minimize the risk of weight gain and related health issues.",
   },
   {
     imageAddress: "/sleep.avif",
     text: "Get Quality Sleep",
     subText:
-      "Aim for 7-9 hours of quality sleep per night. Poor sleep patterns can contribute to insulin resistance.",
+      "Aim for 7-9 hours of quality sleep per night. Poor sleep patterns can contribute to insulin resistance. Getting 7-9 hours of quality sleep each night is essential for maintaining a healthy metabolic balance and reducing the risk of insulin resistance, which is often associated with poor sleep patterns. Prioritizing sleep can significantly benefit your overall health and well-being.",
   },
   {
     imageAddress: "/healthy_diet.jpg",
     text: "Follow a Balanced Diet",
     subText:
-      "Eat a well-balanced diet that includes a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats. Limit the consumption of sugary foods and drinks, as well as foods high in refined carbohydrates",
+      "Eat a well-balanced diet that includes a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats. Limit the consumption of sugary foods and drinks, as well as foods high in refined carbohydrates.",
   },
   {
     imageAddress: "/no_smoke.png",
     text: "Avoid Smoking",
     subText:
-      "Smoking is a risk factor for Type 2 diabetes. If you smoke, seek help to quit.",
+      "Smoking is a risk factor for Type 2 diabetes. If you smoke, seek help to quit. Smoking not only increases the risk of Type 2 diabetes but also exacerbates its complications once the disease is present. Quitting smoking is one of the most significant steps you can take to improve your overall health and reduce the risk of developing diabetes-related complications such as heart disease and kidney problems.",
   },
   {
     imageAddress: "/no_suger.jpg",
     text: "Reduce Sugar Intake",
     subText:
-      "Minimize your consumption of added sugars and sugary beverages. Read food labels to identify hidden sugars.",
+      "Minimize your consumption of added sugars and sugary beverages. Read food labels to identify hidden sugars. It's crucial to be mindful of your added sugar intake, as excessive consumption can contribute to weight gain and insulin resistance. Reading food labels and recognizing hidden sugars in products can empower you to make informed, healthier dietary choices and reduce your risk of Type 2 diabetes.",
   },
 ];
