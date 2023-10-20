5aa6a751b0a98b104408b45c507bd2fa43ef4958
alivarastepour <alivaraste@outlook.com>
2023-10-20 07:10:31 +0330

    fixed char type

--- components/What/What.tsx
+++ components/What/What.tsx
@@ -18,13 +18,13 @@ const What = () => {
           regulates blood sugar (glucose), which is the primary source of energy
           for your cells. There are several types of diabetes, but the two most
           common are type 1 and type 2. Type 1 Diabetes is an autoimmune
-          condition in which the body's immune system mistakenly attacks and
-          destroys the insulin-producing beta cells in the pancreas. Type 2
+          condition in which the body&apos;s immune system mistakenly attacks
+          and destroys the insulin-producing beta cells in the pancreas. Type 2
           Diabetes is the most common form of diabetes and is often associated
           with lifestyle and genetic factors. In Type 2 diabetes, the body
-          doesn't use insulin properly (insulin resistance), and over time, the
-          pancreas may not produce enough insulin. This leads to elevated blood
-          sugar levels.
+          doesn&apos;t use insulin properly (insulin resistance), and over time,
+          the pancreas may not produce enough insulin. This leads to elevated
+          blood sugar levels.
         </div>
         <div className={styles["i"]}>
           <Image alt="a" src={"/w.webp"} width={600} height={500} />
