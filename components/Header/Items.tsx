02005f57aad795ca7bce1934e2f47d16fac381b9
alivarastepour <alivaraste@outlook.com>
2023-10-23 07:34:30 +0330

    renamed a section

--- components/Header/Items.tsx
+++ components/Header/Items.tsx
@@ -4,14 +4,14 @@ const Items = ({ isSmall }: { isSmall: boolean }) => {
   return (
     <>
       <div className={styles["header-item"]}>what</div>
-      <div className={styles["header-item"]}>when</div>
+      {/* <div className={styles["header-item"]}>when</div> */}
       <div className={styles["header-item"]}>how</div>
       <div
         className={`${!isSmall && styles["action-item"]} ${
           styles["header-item"]
         }`}
       >
-        diagnose
+        assess
       </div>
     </>
   );
