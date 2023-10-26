32ac8719a6f1f8f3ee9bb757b0ca9d40cb722bcc
alivarastepour <alivaraste@outlook.com>
2023-10-26 11:45:55 +0330

    moved site logo to a separate component

---
+++ components/shared/Header.tsx
@@ -0,0 +1,22 @@
+import { Tilt_Neon } from "next/font/google";
+import styles from "@/styles/sharedHeader.module.scss";
+import Link from "next/link";
+const tiltNeon = Tilt_Neon({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const SharedHeader = () => {
+  return (
+    <>
+      <div className={`${styles["header-logo"]} ${tiltNeon.className}`}>
+        <Link href={"/"}>
+          <span>Diabetes</span>
+          <span>360</span>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default SharedHeader;
