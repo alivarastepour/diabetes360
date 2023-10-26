eceeff5a67159c1206d5095b391b6588b0e7892e
alivarastepour <alivaraste@outlook.com>
2023-10-26 11:46:22 +0330

    added a header for RiskAssessment page

---
+++ components/RiskAssessmentPageWrapper/Header/Header.tsx
@@ -0,0 +1,18 @@
+import SharedHeader from "@/components/shared/Header";
+import styles from "@/styles/riskAssessmentHeader.module.scss";
+import { Tilt_Neon } from "next/font/google";
+const tiltNeon = Tilt_Neon({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+const Header = () => {
+  return (
+    <>
+      <div className={`${tiltNeon.className} ${styles["header-wrapper"]}`}>
+        <SharedHeader />
+        risk assessment
+      </div>
+    </>
+  );
+};
+export default Header;
