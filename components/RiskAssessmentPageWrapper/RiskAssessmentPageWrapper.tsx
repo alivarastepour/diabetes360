e278f047c8553ea8b9db29b8e8da041addf3d3e4
alivarastepour <alivaraste@outlook.com>
2023-10-26 11:47:02 +0330

    Header is being added to the RiskAssessment page

--- components/RiskAssessmentPageWrapper/RiskAssessmentPageWrapper.tsx
+++ components/RiskAssessmentPageWrapper/RiskAssessmentPageWrapper.tsx
@@ -1,8 +1,10 @@
+import Header from "./Header/Header";
 import Test from "./Test/Test";
 
 const RiskAssessmentPageWrapper = () => {
   return (
     <>
+      <Header />
       <Test />
     </>
   );
