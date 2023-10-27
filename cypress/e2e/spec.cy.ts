9e72dc6ca41c80c1434df7cb66c585d51a47e65c
alivarastepour <alivaraste@outlook.com>
2023-10-27 19:24:53 +0330

    added some tests for Header component

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,28 @@
+import { getTestElement } from "../fixtures/util";
+
 describe("main page tests", () => {
-  it("passes", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000");
+  });
+  it("main logo should exist and upon a click should navigate to homepage.", () => {
+    cy.get(getTestElement("div", "main-logo"))
+      .should("contain.text", "Diabetes360")
+      .click();
+    cy.location("pathname").should("eq", "/");
+  });
+  it("navbar should be wrapped inside hamburger icon when view width is less than 500px. upon click, hamburger menu should toggle visibility.", () => {
+    cy.viewport(499, 900);
+    cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
+    cy.get(getTestElement("div", "hamburger-menu-content"));
+    cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
+    cy.get(getTestElement("div", "hamburger-menu-content")).should("not.exist");
+  });
+  it("hamburger icon should form a cross when clicked for open", () => {
+    cy.viewport(499, 900);
+    cy.get(getTestElement("div", "hamburger-menu-wrapper"))
+      .click()
+      .children()
+      .eq(1)
+      .should("have.css", "display", "none");
   });
 });
---
+++ cypress/fixtures/util.ts
@@ -0,0 +1,2 @@
+export const getTestElement = (tag: string, testID: string) =>
+  `${tag}[data-test-id="${testID}"]`;
