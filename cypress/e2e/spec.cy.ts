df292c2b690cf4819d2b504ef8f9c03bf0b7a445
alivarastepour <alivaraste@outlook.com>
2023-10-29 19:54:01 +0330

    reformatted tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 import { getTestElement } from "../fixtures/util";
 
-describe("main page tests", () => {
+describe("general tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
@@ -10,15 +10,20 @@ describe("main page tests", () => {
       .click();
     cy.location("pathname").should("eq", "/");
   });
-  it("navbar should be wrapped inside hamburger icon when view width is less than 500px. upon click, hamburger menu should toggle visibility.", () => {
+});
+
+describe("small screen tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
     cy.viewport(499, 900);
+  });
+  it("navbar should be wrapped inside hamburger icon. upon click, hamburger menu should toggle visibility.", () => {
     cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
     cy.get(getTestElement("div", "hamburger-menu-content"));
     cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
     cy.get(getTestElement("div", "hamburger-menu-content")).should("not.exist");
   });
   it("hamburger icon should form a cross when clicked for open", () => {
-    cy.viewport(499, 900);
     cy.get(getTestElement("div", "hamburger-menu-wrapper"))
       .click()
       .children()
