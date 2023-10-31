fa2a040bd833e6525caed836fe90fb42ac7ce125
alivarastepour <alivaraste@outlook.com>
2023-10-31 20:58:20 +0330

    added tests for compact mode assessment

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,59 +1,59 @@
 import { getTestElement } from "../fixtures/util";
 
-// describe("general tests", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000");
-//   });
-//   it("main logo should exist and upon a click should navigate to homepage.", () => {
-//     cy.get(getTestElement("div", "main-logo"))
-//       .should("contain.text", "Diabetes360")
-//       .click();
-//     cy.location("pathname").should("eq", "/");
-//   });
-//   it("test button should navigate to its corresponding path.", () => {
-//     cy.get(getTestElement("div", "take-test-wrapper")).click();
-//     cy.location("pathname").should("eq", "/RiskAssessment");
-//   });
-// });
+describe("general tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+  it("main logo should exist and upon a click should navigate to homepage.", () => {
+    cy.get(getTestElement("div", "main-logo"))
+      .should("contain.text", "Diabetes360")
+      .click();
+    cy.location("pathname").should("eq", "/");
+  });
+  it("test button should navigate to its corresponding path.", () => {
+    cy.get(getTestElement("div", "take-test-wrapper")).click();
+    cy.location("pathname").should("eq", "/RiskAssessment");
+  });
+});
 
-// describe("small screen tests", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000");
-//     cy.viewport(499, 900);
-//   });
-//   it("navbar should be wrapped inside hamburger icon. upon click, hamburger menu should toggle visibility.", () => {
-//     cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
-//     cy.get(getTestElement("div", "hamburger-menu-content"));
-//     cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
-//     cy.get(getTestElement("div", "hamburger-menu-content")).should("not.exist");
-//   });
-//   it("hamburger icon should form a cross when clicked for open", () => {
-//     cy.get(getTestElement("div", "hamburger-menu-wrapper"))
-//       .click()
-//       .children()
-//       .eq(1)
-//       .should("have.css", "display", "none");
-//   });
-//   it("background repeat should have the value 'repeat'.", () => {
-//     cy.get(getTestElement("div", "hero-background")).should(
-//       "have.css",
-//       "background-repeat",
-//       "repeat"
-//     );
-//   });
-//   it("background should be blurry.", () => {
-//     cy.get(getTestElement("div", "hero-background")).should(
-//       "not.have.css",
-//       "filter",
-//       ""
-//     );
-//   });
-//   it("prevention section should be scrollable.", () => {
-//     cy.get(getTestElement("div", "how-slider-wrapper"))
-//       .should("not.have.css", "scroll", "hidden")
-//       .should("have.css", "scroll-snap-type");
-//   });
-// });
+describe("small screen tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.viewport(499, 900);
+  });
+  it("navbar should be wrapped inside hamburger icon. upon click, hamburger menu should toggle visibility.", () => {
+    cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
+    cy.get(getTestElement("div", "hamburger-menu-content"));
+    cy.get(getTestElement("div", "hamburger-menu-wrapper")).click();
+    cy.get(getTestElement("div", "hamburger-menu-content")).should("not.exist");
+  });
+  it("hamburger icon should form a cross when clicked for open", () => {
+    cy.get(getTestElement("div", "hamburger-menu-wrapper"))
+      .click()
+      .children()
+      .eq(1)
+      .should("have.css", "display", "none");
+  });
+  it("background repeat should have the value 'repeat'.", () => {
+    cy.get(getTestElement("div", "hero-background")).should(
+      "have.css",
+      "background-repeat",
+      "repeat"
+    );
+  });
+  it("background should be blurry.", () => {
+    cy.get(getTestElement("div", "hero-background")).should(
+      "not.have.css",
+      "filter",
+      ""
+    );
+  });
+  it("prevention section should be scrollable.", () => {
+    cy.get(getTestElement("div", "how-slider-wrapper"))
+      .should("not.have.css", "scroll", "hidden")
+      .should("have.css", "scroll-snap-type");
+  });
+});
 
 describe("assessment introduction dialog tests", () => {
   beforeEach(() => {
@@ -92,5 +92,33 @@ describe("assessment introduction dialog tests", () => {
       .should("have.css", "display", "none")
       .should("have.css", "opacity", "0");
     cy.get(getTestElement("button", "start-test")).should("be.hidden");
+  });
+});
+
+describe("assessment tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/RiskAssessment");
+    cy.get(getTestElement("div", "test-intro-select")).click();
+    cy.get(getTestElement("button", "start-test")).click();
+  });
+  it("compact test: normal behavior", () => {
+    let i = 1;
+    cy.get(getTestElement("input", `input-radio-${i++}-${0}`)).click();
+    cy.get(getTestElement("button", "next-question-button")).click();
+    cy.wait(1000);
+    cy.get(getTestElement("input", `input-radio-${i++}-${1}`)).click();
+    cy.get(getTestElement("button", "next-question-button")).click();
+    cy.wait(1000);
+    cy.get(getTestElement("input", `input-number-${i++}`)).type("12");
+    cy.get(getTestElement("button", "next-question-button")).click();
+    cy.wait(1000);
+    cy.get(getTestElement("input", `input-radio-${i++}-${3}`)).click();
+    cy.get(getTestElement("button", "next-question-button")).click();
+    cy.wait(1000);
+    cy.get(getTestElement("input", `input-radio-${i++}-${1}`)).click();
+    cy.get(getTestElement("button", "next-question-button")).click();
+    cy.wait(1000);
+    cy.get(getTestElement("input", `input-radio-${i++}-${7}`)).click();
+    cy.get(getTestElement("button", "submit-question-button")).click();
   });
 });
