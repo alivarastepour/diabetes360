4488977c1553c99e55789241d7e26766fdfc1550
alivarastepour <alivaraste@outlook.com>
2023-11-02 07:44:56 +0330

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DWSVN55RGN"
+      ></Script>
+      <Script>
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DWSVN55RGN');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
